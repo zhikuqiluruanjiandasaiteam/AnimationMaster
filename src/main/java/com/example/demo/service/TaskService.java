dae5fe8053fe6ed337b5a765ef28a7992f4def09
tom <tom@123.com>
2019-08-02 14:59:19 +0800

    添加属性

--- src/main/java/com/example/demo/service/TaskService.java
+++ src/main/java/com/example/demo/service/TaskService.java
@@ -5,8 +5,10 @@ import com.example.demo.dao.FilesMapper;
 import com.example.demo.dao.TaskMapper;
 import com.example.demo.entity.Files;
 import com.example.demo.entity.Task;
+import com.example.demo.util.RemoteShellExecutor;
 import org.omg.PortableInterceptor.INACTIVE;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +21,21 @@ public class TaskService {
 
     @Autowired
     private TaskMapper taskMapper;
+
+    @Value("${DLServerIP}")
+    private String DLServerIP;
+
+    @Value("${DLServerUserName}")
+    private String DLServerUserName;
+
+    @Value("${DLServerPassword}")
+    private String DLServerPassword;
+
+    @Value("${DLServerSecretKey}")
+    private String DLServerSecretKey;
+
+    @Value("${ExecShell}")
+    private String ExecShell;
 
     /**
      * 创建任务（仅创建记录，未开启任务）
@@ -47,6 +64,7 @@ public class TaskService {
         taskMapper.insertGetId( task );
         return task.getTaskId();
     }
+
 
 
 }
